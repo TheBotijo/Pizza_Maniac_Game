87baf61fe80ffc8f86d72bbaf68d3ee10fc6ef4e
martifontanet <marti.fontanet@estudiantat.upc.edu>
2023-05-23 23:27:19 +0200

    Player en proceso

--- Assets/Scripts/Player/PlayerCharacter.cs
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerCharacter : MonoBehaviour
@@ -11,15 +12,18 @@ public class PlayerCharacter : MonoBehaviour
 	}
     //New Input system
     public PlayerInputMap _playerInput;
+    Rigidbody rb;
 
+    [Header("Movement")]
+    private float lateSpeed;
     public Transform cameraRoot;
     public float moveSpeed = 5f;
     public bool runEnabled = true;
     public float runSpeed = 10f;
     public float gravity = -9.8f; //-13 works ok
     public FacingOptions facing = FacingOptions.MovementDirection;
+    public bool aiming;
 
-    private CharacterController characterController;
     private Vector3 direction;
     private Vector3 cameraForwardProjected;
     private float speed;
@@ -41,25 +45,31 @@ public class PlayerCharacter : MonoBehaviour
     private float jumpY0;
     private Vector3 jumpDirection;
 
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
     bool grounded;
     public float groundDrag;
 
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        lateSpeed = moveSpeed;
         speed = moveSpeed;
         _playerInput = new PlayerInputMap();
         _playerInput.Juego.Enable();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculateDirection();
+        PlayMove();
         if (facing != FacingOptions.None) Facing(facing);
 
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight, whatIsGround);
+        //Para implementar la deteccion del suelo
+        //grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight, whatIsGround);
 
         //Movement zone
         {
@@ -77,7 +87,6 @@ public class PlayerCharacter : MonoBehaviour
             movement = Time.deltaTime * direction * speed;
             movement.y = jumping ? GetJumpCurrentHeight() : Time.deltaTime * gravity;
 
-            characterController.Move(movement);
         }
 
         //Jump zone.
@@ -136,7 +145,41 @@ public class PlayerCharacter : MonoBehaviour
         transform.forward = direction;
 	}
 
-	private void CalculateDirection()
+    private void PlayMove()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        //apliquem una força al moviment quan esta tocant al terra
+        if (aiming)
+            moveSpeed = lateSpeed / 2;
+        else
+            moveSpeed = lateSpeed;
+
+        if (grounded && _playerInput.Juego.Run.IsPressed())
+        {
+            rb.AddForce(direction.normalized * moveSpeed * 20f, ForceMode.Force);
+
+        }
+        else if (grounded)
+        {
+            rb.AddForce(direction.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        }
+        else if (!grounded && flatVel.magnitude == 0)
+        {
+            rb.AddForce(direction.normalized, ForceMode.Force);
+        }
+        else if (!grounded && flatVel.magnitude > (moveSpeed * 8)) //a l'aire
+        {
+            rb.AddForce(direction.normalized * moveSpeed * 20f, ForceMode.Force);
+        }
+        else if (!grounded) //a l'aire
+        {
+            rb.AddForce(direction.normalized * moveSpeed * 10f , ForceMode.Force);
+        }
+
+    }
+
+    private void CalculateDirection()
 	{
         float horizontal = _playerInput.Juego.Move.ReadValue<Vector2>().x;
         float vertical = _playerInput.Juego.Move.ReadValue<Vector2>().y;
@@ -167,5 +210,18 @@ public class PlayerCharacter : MonoBehaviour
         float currentJumpHeight = jumpY0 + jumpHeight * jumpRaiseCurve.Evaluate(curveTime);
 
         return currentJumpHeight - transform.position.y;
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limitar la velocitat si aquesta es mes gran del que volem aconseguir
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            //tornem a aplicar aquesta nova velocitat al player
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+
     }
 }
