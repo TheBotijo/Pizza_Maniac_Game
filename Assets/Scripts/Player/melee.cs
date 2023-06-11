1565930def7e90b41ae4e5b259f25d82c0cdae9c
Luis Chaparro <luis.chaparro@estudiantat.upc.edu>
2023-06-11 17:33:23 +0200

    FarolasBoca

--- Assets/Scripts/Player/melee.cs
+++ Assets/Scripts/Player/melee.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class Melee : MonoBehaviour
 {
     private Shooting shootScr;
+    [SerializeField]
+    private ParticlesAgua aguaScr;
     private void OnTriggerEnter(Collider other)
     {
         shootScr = gameObject.GetComponentInParent<Shooting>();
+       
         if (other.CompareTag("Enemy"))
         {
             if (other.gameObject.name == "BichoQueso(Clone)")
@@ -27,11 +31,32 @@ public class Melee : MonoBehaviour
             }
 
             Invoke(nameof(ReseteShot), shootScr.timeBetweenShooting);
+        }else if (other.CompareTag("Farola")){
+            other.GetComponent<ParticlesMetal>().metal.Play();
+            other.GetComponent<ParticlesMetal>().chispaSound.Play();
+            other.GetComponent<ParticlesMetal>().Chispas.Play();
+            Debug.Log("chispas");
+        }
+        else if (other.CompareTag("BocaIncendio"))
+        {
+            aguaScr = other.GetComponent<ParticlesAgua>();
+            aguaScr.metal.Play();
+            aguaScr.Agua.Play();
+            aguaScr.aguaSound.Play();
+            Debug.Log("agua");
+            Invoke(nameof(Stop), 5f);
+        }
+    }
+    void Stop()
+        {
+            Debug.Log("noagua");
+            aguaScr.Agua.Stop();
+            aguaScr.aguaSound.Stop();
         }
 
         void ReseteShot()
         {
             shootScr.ResetShot();
         }
-    }
+    
 }
